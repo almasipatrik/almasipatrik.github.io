510b2d317c371c22ff21dabc4388205a48677d85
CD1H49 <almasi.patrik@gmail.com>
2021-01-17 22:33:03 +0100

    Updated.

--- site.js
+++ site.js
@@ -1,23 +1,32 @@
-$(function() {
+$(function () {
   AOS.init();
 });
 
-$(document).ready(function() {
-  const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-  const mobile_menu = document.getElementById('hamburger-list');
-  const header = document.querySelector('.header.container');
+var hamburger;
+var mobile_menu;
+var header;
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+$(document).ready(function () {
+  hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+  mobile_menu = document.getElementById("hamburger-list");
+  header = document.querySelector(".header.container");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
   });
 
-  document.addEventListener('scroll', () => {
+  document.addEventListener("scroll", () => {
     var scroll_position = window.scrollY;
-    if(scroll_position > 250) {
+    if (scroll_position > 250) {
       header.style.backgroundColor = "#29323c";
-    }else{
+    } else {
       header.style.backgroundColor = "transparent";
     }
   });
 });
+
+function toggleHamburger() {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle("active");
+}
